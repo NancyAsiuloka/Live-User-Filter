fbbeb7cb5282b21565d284d34a89090e4cdeece8
Nancy Asiuloka <nancyasiuloka@gmail.com>
2023-05-28 10:30:49 +0100

    added js

--- script.js
+++ script.js
@@ -12,7 +12,21 @@ async function getData(){
     // clear result
     results.innerHTML = '';
 
-    data.results
+    results.forEach(user => {
+        const li = document.createElement('li');
+
+        listItems.push(li);
+
+        li.innerHTML = `
+            <img src="${user.picture.large}" alt="${user.name.first}"/>
+            <div class="user-info">
+                <h4>${user.name.first} ${user.name.last}</h4>
+                <p>${user.location.city}, ${user.location.country}</p>
+            </div>
+        `;
+
+        result.appendChild(li);
+    })
 }
 
 
