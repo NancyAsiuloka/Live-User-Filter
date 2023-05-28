acefbfedcba056ebeae87913cc490a3f50b2c34d
Nancy Asiuloka <nancyasiuloka@gmail.com>
2023-05-28 10:20:32 +0100

    added js

--- script.js
+++ script.js
@@ -2,12 +2,17 @@ const result = document.getElementById('result');
 const filter = document.getElementById('filter');
 const listItems = [];
 
+getData();
+
 async function getData(){
     const res = await fetch('https://randomuser.me/api?results=50');
 
-    const data = await res.json();
+    const { results} = await res.json();
 
-    console.log(data);
+    // clear result
+    results.innerHTML = '';
+
+    data.results
 }
 
 
